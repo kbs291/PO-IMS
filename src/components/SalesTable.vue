01ac3fb4c4da002067c75378853056e8de071674
kbs291 <kristian.suroy291@gmail.com>
2025-07-19 11:58:00 +0800

    refactor: update src/components/SalesTable.vue
    - change tab structure to button
    - add a function to toggle the month filter if 'By Month' is selected
    
    style: update the 'All/By Month' button style to fit the theme color

--- src/components/SalesTable.vue
+++ src/components/SalesTable.vue
@@ -16,7 +16,7 @@ const props = defineProps({
 const getSales = computed(() => {
   const date = new Date(month.value);
   if (tab.value === 'month') {
-    return props.sales.filter(sale => {
+    return props.sales.filter((sale) => {
       const saleDate = new Date(sale.purchaseDate);
       return saleDate.getMonth() === date.getMonth() && saleDate.getFullYear() === date.getFullYear();
     });
@@ -26,89 +26,57 @@ const getSales = computed(() => {
 </script>
 
 <template>
-  <div class="d-flex align-items-center justify-content-between">
+  <div class="d-flex justify-content-between align-items-center">
     <h4 class="m-0">PO Card Sales</h4>
-    <nav>
-      <div class="nav nav-tabs justify-content-end border-0" id="nav-tab" role="tablist">
-        <button class="nav-link border-0 active" id="nav-all-tab" data-bs-toggle="tab" data-bs-target="#nav-all" type="button" role="tab" aria-controls="nav-all" aria-selected="true" @click="tab = 'all'">All</button>
-        <button class="nav-link border-0" id="nav-due-dates-tab" data-bs-toggle="tab" data-bs-target="#nav-due-dates" type="button" role="tab" aria-controls="nav-due-dates" aria-selected="false" @click="tab = 'month'">By Month</button>
-      </div>
-    </nav>
+    <div class="btn-group">
+      <button type="button" class="border-0" :class="{ 'active': tab === 'all' }" @click="tab = 'all'">All</button>
+      <button type="button" class="border-0" :class="{ 'active': tab === 'month' }" @click="tab = 'month'">By Month</button>
+    </div>
   </div>
 
-  <div class="tab-content" id="nav-tabContent">
-    <div class="tab-pane fade show active" id="nav-all" role="tabpanel" aria-labelledby="nav-all-tab" tabindex="0">
-      <div class="table-responsive" v-if="getSales.length > 0">
-        <table class="table table-hover align-middle mt-4">
-          <thead>
-            <tr>
-              <th scope="col">ID</th>
-              <th scope="col">Name</th>
-              <th scope="col">Purchase Date</th>
-              <th scope="col">Number of Cards</th>
-              <th scope="col" class="text-end">Total Amount</th>
-            </tr>
-          </thead>
-          <tbody class="table-group-divider">
-            <tr v-for="sale in getSales" :key="sale.id">
-              <td>{{ sale.id }}</td>
-              <td>{{ sale.name }}</td>
-              <td>{{ formatDate(sale.purchaseDate) }}</td>
-              <td>{{ sale.numberOfCards }}</td>
-              <td class="text-end">{{ formatAmount(sale.totalAmount) }}</td>
-            </tr>
-          </tbody>
-          <tfoot>
-            <tr>
-              <td colspan="4">&nbsp;</td>
-              <td class="text-end">{{ formatAmount(calculateTotal(getSales, 'totalAmount')) }}</td>
-            </tr>
-          </tfoot>
-        </table>
-      </div>
-      <div class="p-5 text-center border rounded empty" v-else>No Data</div>
+  <div>
+    <div class="d-flex justify-content-end mt-4 mb-2" v-if="tab === 'month'">
+      <input type="month" v-model="month" />
     </div>
 
-    <div class="tab-pane fade" id="nav-due-dates" role="tabpanel" aria-labelledby="nav-due-dates-tab" tabindex="0">
-      <div class="d-flex justify-content-end mt-3 mb-2">
-        <input type="month" v-model="month">
-      </div>
-
-      <div class="table-responsive" v-if="getSales.length > 0">
-        <table class="table table-hover align-middle mt-2">
-          <thead>
-            <tr>
-              <th scope="col">ID</th>
-              <th scope="col">Name</th>
-              <th scope="col">Purchase Date</th>
-              <th scope="col">Number of Cards</th>
-              <th scope="col" class="text-end">Total Amount</th>
-            </tr>
-          </thead>
-          <tbody class="table-group-divider">
-            <tr v-for="sale in getSales" :key="sale.id">
-              <td>{{ sale.id }}</td>
-              <td>{{ sale.name }}</td>
-              <td>{{ formatDate(sale.purchaseDate) }}</td>
-              <td>{{ sale.numberOfCards }}</td>
-              <td class="text-end">{{ formatAmount(sale.totalAmount) }}</td>
-            </tr>
-          </tbody>
-          <tfoot>
-            <tr>
-              <td colspan="4">&nbsp;</td>
-              <td class="text-end">{{ formatAmount(calculateTotal(getSales, 'totalAmount')) }}</td>
-            </tr>
-          </tfoot>
-        </table>
-      </div>
-      <div class="p-5 text-center border rounded empty" v-else>No Data</div>
+    <div class="table-responsive" v-if="getSales.length > 0">
+      <table class="table table-hover align-middle mt-2">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Name</th>
+            <th scope="col">Purchase Date</th>
+            <th scope="col">Number of Cards</th>
+            <th scope="col" class="text-end">Total Amount</th>
+          </tr>
+        </thead>
+        <tbody class="table-group-divider">
+          <tr v-for="sale in getSales" :key="sale.id">
+            <td>{{ sale.id }}</td>
+            <td>{{ sale.name }}</td>
+            <td>{{ formatDate(sale.purchaseDate) }}</td>
+            <td>{{ sale.numberOfCards }}</td>
+            <td class="text-end">{{ formatAmount(sale.totalAmount) }}</td>
+          </tr>
+        </tbody>
+        <tfoot>
+          <tr>
+            <td colspan="4">&nbsp;</td>
+            <td class="text-end">{{ formatAmount(calculateTotal(getSales, 'totalAmount')) }}</td>
+          </tr>
+        </tfoot>
+      </table>
     </div>
+    <div class="p-5 text-center border rounded empty" v-else>No Data</div>
   </div>
 </template>
 
 <style scoped>
-.nav-link {
+.btn-group button {
   color: var(--bs-secondary);
+  background: none;
+  &.active {
+    color: rgba(0, 0, 0, 1);
+  }
 }
 </style>
