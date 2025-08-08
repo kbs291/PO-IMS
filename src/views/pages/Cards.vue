12f6315146a5298806bf9416ac2b45c0c4aa4ea4
kbs291 <kristian.suroy291@gmail.com>
2025-08-08 11:59:31 +0800

    feature: create CardsTable component to display the data
    - add a dummy data on Cards component
    - create CardsTable component and use primevue DataTable
    - setup CardsTable to accept props from the parent component

--- src/views/pages/Cards.vue
+++ src/views/pages/Cards.vue
@@ -1,10 +1,23 @@
-<script setup></script>
+<script setup>
+import CardsTable from '@/components/table/CardsTable.vue';
+import { reactive } from 'vue';
+
+const cards = reactive([
+  {
+    id: 1,
+    code: 'A72DJW5XQK',
+    addedDate: new Date(),
+    availability: true,
+    purchaseDate: ''
+  }
+]);
+</script>
 
 <template>
   <div class="card">
     <div class="card-title">Cards</div>
     <div class="card-content">
-      Content
+      <CardsTable :cards />
     </div>
   </div>
 </template>
